984703d4618b0e278ea8107d1d49970a5841d356
cun <354073878@qq.com>
2018-07-14 13:38:13 +0800

    1

---
+++ src/main/java/com/cun/SpringBootSolrApplication.java
@@ -0,0 +1,12 @@
+package com.cun;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootSolrApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootSolrApplication.class, args);
+	}
+}
